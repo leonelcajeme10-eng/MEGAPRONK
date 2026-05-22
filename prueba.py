bf2ff0bc9259f2bee6778c6a744c3c89d3eff117
LeoFeo <leonelcajeme10@gmail.com>
2026-05-22 10:08:30 -0700

    proyectada

--- prueba.py
+++ prueba.py
@@ -1,2 +1,3 @@
 print("Hola soy Pablo y me gusta el gore")
-print("veras niu... el contexto es el siguiente, toda mi vida he sido un")
+print("veras niu... el contexto es el siguiente, toda mi vida he sido un frcasado sin remedio xd")
+print("Mi suerte genetica se decidio hace mucho pana xd")

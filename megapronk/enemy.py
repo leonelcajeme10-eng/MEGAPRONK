99e8a95a0ad12e823fe23f864749dfe0bcb7d8e8
sebastian89011-wq <sebastian89011@gmail.com>
2026-05-25 22:40:52 -0700

    leo neo
    v i o l a  d                   o            r

--- megapronk/enemy.py
+++ megapronk/enemy.py
@@ -31,15 +31,17 @@ class Enemy:
 
             mov_x = self.dx * self.speed * dt
             mov_y = self.dy * self.speed * dt    
-            self.x += mov_x
+            
             self.y += mov_y
             
 
             if mov_x != 0:
+                self.x += mov_x
                 self.boton_x = True
                 self.colisiones(mapa_rect)
 
             if mov_y != 0:
+                self.x += mov_x
                 self.boton_y = True
                 self.colisiones(mapa_rect)
             

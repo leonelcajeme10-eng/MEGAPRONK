b9632f5bc31b558b09d71c97ccdcac01edc63360
sebastian89011-wq <sebastian89011@gmail.com>
2026-05-29 20:09:32 -0700

    dfefdef
    fefef

--- megapronk/enemigo_mariposa.py
+++ megapronk/enemigo_mariposa.py
@@ -3,11 +3,11 @@ from player import Player
 import math
 import random
 from camara import Camara
+import Enemy
 
 
 
-
-class Fantasma:
+class Mariposa(Enemy):
     COLOR = "red"
     SPEED = 120
     VIDA = 100
@@ -15,6 +15,13 @@ class Fantasma:
     TAMANO_X = 50
     TAMANO_Y = 50
 
-    __init__(self,camara):
+    def __init__(self,camara):
         super().__init__()
+        self.tamano_x = self.TAMANO_X
+        self.tamano_y = self.TAMANO_Y
+        self.vida = self.VIDA
+        self.speed = self.SPEED
+        self.damage = self.DAMAGE
+
+
         
